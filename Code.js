e962d4d1c6a53579e1379a2d67a1141903b1f733
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-10-14 23:31:01 -0400

    feat: add all potential keys

--- Code.js
+++ Code.js
@@ -39,7 +39,28 @@ function myFunction() {
   for (let key of Object.keys(events)) {
     const sheet = ss.getSheetByName(key)
     const event = events[key]
-    sheet.appendRow([yesterday, getValue('gen',event), getValue('cdsdemo', event), getValue('ONApi', event)])
+
+    sheet.appendRow([yesterday, 
+      getValue('gen', event),
+      getValue('cdsdemo', event),
+      getValue('CDSSA', event),
+      getValue('AB', event),
+      getValue('BC', event),
+      getValue('MB', event),
+      getValue('NB', event),
+      getValue('NL', event),
+      getValue('NS', event),
+      getValue('NT', event),
+      getValue('NU', event),
+      getValue('ONApi', event),
+      getValue('ONPortal', event),
+      getValue('PE', event),
+      getValue('QC', event),
+      getValue('QCApiDemo', event),
+      getValue('SK', event),
+      getValue('YT', event),
+      getValue('CAF', event),
+    ])
   }
 
 }
