2edca9f6115adc8243174f65fd6c9b29d93ddc58
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-10-28 11:35:00 -0400

    prepopulating inline

---
+++ inline/Code.js
@@ -0,0 +1,51 @@
+function fetchMetric(date, url,  creds)
+{ 
+  const response = UrlFetchApp.fetch(`${url}${date}`, {
+    'muteHttpExceptions': false,
+    'headers': {
+      'Authorization': `Basic ${creds}`
+    },});
+  return JSON.parse(response.getContentText())
+}
+
+function parseJson(json){
+  const events = {
+    "OTKClaimed": [],
+    "OTKUnclaimed": [],
+    "OTKRegenerated": [],
+    "OTKGenerated": [],
+    "OTKExpired": [],
+    "OTKExhausted": []
+  }
+  json.forEach(function(x) {
+    events[x.identifier].push({
+      source : x.source,
+      count : x.count
+    })
+  })
+  return events
+}
+
+/*
+* @param {string[]} lookupKeys - The list of province identifiers to lookup
+* @param {string} url - The metric endpoint
+* @param {string} creds - Base64 Encoded string for basic auth format of encoded string is uname:pword
+*
+*/
+function updateSheet(url, creds, date) {
+
+  //const yesterday = Utilities.formatDate(new Date(new Date().getTime() - 86400000),'EST', 'yyyy-MM-dd')
+  const ss = SpreadsheetApp.getActiveSpreadsheet()
+  const json = fetchMetric(date, url, creds)
+  const events = parseJson(json)
+
+  for (let key of Object.keys(events)) {
+    const sheet = ss.getSheetByName(key)
+    const event = events[key]
+
+    event.forEach(e => {
+      sheet.appendRow([date, e.source, e.count])
+    })
+  }
+
+}
